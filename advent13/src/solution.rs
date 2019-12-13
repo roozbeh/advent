8c568a25a1169b0ef3ccf4b7ddfe0b7917ccb99d
dialtone <dialtone@adroll.com>
2019-12-13 13:53:44 -0800

    nicer graphics

--- advent13/src/solution.rs
+++ advent13/src/solution.rs
@@ -1,6 +1,8 @@
 use crate::intcode;
 use std::collections::HashMap;
 use std::fmt::Display;
+use std::thread::sleep;
+use std::time::Duration;
 
 pub fn part1(input: &str) -> impl Display {
     let mut board = HashMap::new();
@@ -62,21 +64,19 @@ fn board_size(input: &str) -> (usize, usize) {
 }
 
 pub fn part2(input: &str) -> impl Display {
+    clear_screen();
     let (max_x, max_y) = board_size(input);
 
     let mut computer = intcode::Computer::from(input);
     computer.store_mem(0, 2);
 
     let mut board = vec![vec![0; max_x]; max_y];
-    //let mut board = HashMap::new();
     let mut joystick_position = 0;
     let mut current_ball_position = (0, 0);
     let mut current_paddle_position = (0, 0);
     let mut score = 0;
     while !computer.has_halted() {
-        // let mut horizontal_paddle = vec![];
         if computer.is_waiting() {
-            println!("input {} score {}", joystick_position, score);
             computer.write_input(joystick_position);
             computer.resume();
         } else {
@@ -113,25 +113,10 @@ pub fn part2(input: &str) -> impl Display {
             }
             board[y as usize][x as usize] = tile_id;
         }
+        go_to_top();
+        println!("score {} input {}     ", score, joystick_position);
         print_board(&board);
-        // let horizontal_paddle = board.iter().filter(|&(_, &v)| v == 3).collect::<Vec<_>>();
-        // if horizontal_paddle
-        //     .iter()
-        //     .filter(|&(k, _)| k.1 == current_ball_position.1)
-        //     .count()
-        //     == 0
-        // {
-        //     // the paddle is not under the ball
-        //     let rightest_paddle_y = horizontal_paddle.iter().map(|&(k, _)| k.1).max().unwrap();
-        //     let leftest_paddle_y = horizontal_paddle.iter().map(|&(k, _)| k.1).min().unwrap();
-        //     if current_ball_position.1 > rightest_paddle_y {
-        //         joystick_position = 1
-        //     } else if current_ball_position.1 < leftest_paddle_y {
-        //         joystick_position = -1
-        //     }
-        // } else {
-        //     joystick_position = 0
-        // }
+        sleep(Duration::from_millis(25));
     }
     score
 }
@@ -139,12 +124,20 @@ pub fn part2(input: &str) -> impl Display {
 fn tile_to_string(c: &isize) -> String {
     match c {
         0 => " ".to_owned(),
-        1 => "W".to_owned(),
-        2 => "#".to_owned(),
-        3 => "=".to_owned(),
-        4 => "o".to_owned(),
+        1 => "█".to_owned(),
+        2 => "▒".to_owned(),
+        3 => "▬".to_owned(),
+        4 => "●".to_owned(),
         _ => "".to_owned(),
     }
+}
+
+fn go_to_top() {
+    print!("{}[0;0H", 27 as char);
+}
+
+fn clear_screen() {
+    print!("{}[2J", 27 as char);
 }
 
 fn print_board(board: &Vec<Vec<isize>>) {
